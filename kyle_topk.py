b53b9650c77c5fa6ce241aba8f05589d7c611333
James Ellis <156376327+Jae-Solow@users.noreply.github.com>
2024-03-16 20:20:28 -0400

    Update kyle_topk.py

--- kyle_topk.py
+++ kyle_topk.py
@@ -15,6 +15,67 @@ if "deep cleaning" in cleaning_services:
     total_price += 50
 if "carpet cleaning" in cleaning_services:
     total_price += 30
+if "office cleaning" in cleaning_services:
+    total_price +=
+if "club house and halls" in cleaning_services:
+    total_price +=
+if "eateries and kitchens" in cleaning_services:
+    total_price +=
+if "initial clean up" in cleaning_services:
+    total_price += 
+if "sparkle clean" in cleaning_services:
+    total_price += 
+if "washroom floors" in cleaning_services:
+    total_price +=
+if "common floors" in cleaning_services:
+    total_price +=
+if "courtyard cleaning" in cleaning_services:
+    total_price +=
+if "common restroom" in cleaning_services:
+    total_price +=
+if "trash clean up" in cleaning_services:
+    total_price +=
+if "containment area cleaning" in cleaning_services:
+    total_price +=
+
+if "move in" || "move out" in cleaning_services:
+    total_price +=
+if "kitchen appliances" in cleaning_services:
+    total_price +=
+if "inside" || "outside in cleaning_services:
+    total_price +=
+if "spring cleaning" in cleaning_services:
+    total_price +=
+if "junk removel" in cleaning_services:
+    total_price +=
+if "detail dusting" in cleaning_services:
+    total_price +=
+if "one time cleaning" in cleaning_services:
+    total_price +=
+if "recurring cleaning" in cleaning_services:
+#conditional for number of times and frequency of recurrence 
+
+if "common area cleaning" in cleaning_services:
+    total_price +=
+if "bathroom floors" in cleaning_services:
+    total_price +=
+if "bathtub cleaning" in cleaning_services:
+    total_price +=
+if "pipeline cleaning" in cleaning_services:
+    total_price +=
+
+if "debris removal" in cleaning_services:
+    total_price +=
+if "dust removal" in cleaning_services:
+    total_price +=
+if "sweeping and mopping" in cleaning_services:
+    total_price +=
+if "wall cleaning" in cleaning_services:
+    total_price +=
+if "fixture cleaning" in cleaning_services:
+    total_price +=
+if "floor care" in cleaning_services:
+    total_price +=
 
 # Display the final price to the user
 print("The final price for the cleaning job is: $", total_price)
